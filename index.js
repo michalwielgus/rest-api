8b15c0bc3022b644c8362b653ec718a7a21ae6f8
Michal Mieculewicz Wielgus <wielgus.michaal@gmail.com>
2021-01-04 23:40:43 +0100

    01 XHR finished

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-// https://api.punkapi.com/v2/beers
+const API_URL = 'https://api.punkapi.com/v2/beers';
+const container = document.querySelector('.container');
 
+const render = (data) => {
+  if (!data.length) return;
+  const fragment = document.createDocumentFragment();
+  data.forEach((beer) => {
+    const div = document.createElement('div');
+    div.classList.add('beer');
+    div.innerHTML = `
+        <div class="beer--content">
+            <h1 class="beer--title">${beer.name}</h1>
+            <p class="beer--tagline">${beer.tagline}</p>
+            <p class="beer--description">${beer.description}</p>
+        </div>
+        <img class="beer--image" src="${beer.image_url}" alt="${beer.name}">
+      `;
+    console.log(beer);
+    fragment.appendChild(div);
+  });
+
+  container.appendChild(fragment);
+};
+
+const success = (data) => {
+  const beers = JSON.parse(data.target.responseText);
+  render(beers);
+};
+
+const error = (error) => {
+  console.log(error);
+};
+
+const req = new XMLHttpRequest();
+req.onload = success;
+req.onerror = error;
+req.open('GET', API_URL);
+req.send();
